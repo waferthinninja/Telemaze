e5f976a0e4dbfcfd2f24b2b0ce2b97a007e92d66
waferthinninja <waferthinninja@gmail.com>
2017-12-23 13:24:52 +0000

    Basics stuff set up

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float Speed = 2f;
+
+    public float TeleportTimeout = 0f;
+
+    public float TimeoutLength = 2f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 inputDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        transform.Translate(inputDir * Time.deltaTime * Speed);
+
+        TeleportTimeout -= Time.deltaTime;
+	}
+
+    public void StartTimeout()
+    {
+        TeleportTimeout = TimeoutLength;
+    }
+}
---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour {
+
+    public Transform target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            var player = col.gameObject.GetComponent<PlayerController>();
+            if (player.TeleportTimeout <= 0f)
+            {
+                player.StartTimeout();
+                col.transform.position = target.position;
+            }
+        }
+
+    }
+}
